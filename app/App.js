d4a0fdcba78f5b89c5cd1d067abbe3f26dbf8d5c
yeyoung <yangyeyoung13@gmail.com>
2025-02-16 04:24:26 +0900

    refactor: 앱 로직 수정 및 코드 정리

---
+++ app/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignUpStartScreen from './screens/SignUp/SignUpStart';
+
+const App = () => {
+    return (
+      <SignUpStartScreen></SignUpStartScreen>
+    );
+  };
+  
+  export default App;
