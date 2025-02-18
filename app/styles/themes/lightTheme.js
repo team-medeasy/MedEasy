939fa20559695837efa6628a73295e977cffeb4d
kimgazii <gy0320ya@naver.com>
2025-02-18 23:22:49 +0900

    style: lightTheme 컬러 추가

--- app/styles/themes/lightTheme.js
+++ app/styles/themes/lightTheme.js
@@ -1,13 +1,17 @@
 export const lightTheme = {
     textColor: {
         textPrimary: '#000000',
+        Primary20: '#00000033',
         buttonText: '#ffffff',
     },
     boxColor: {
         buttonPrimary: '#000000',
+        buttonSecondary: '#ffffff',
         inputPrimary: '#0000000A',
     },
     bgColor: {
+        bgPrimary: '#ffffff',
+        bgSecondary: '#f5f5f5',
     },
     borderColor: {
         borderPrimary: '#0000001A',
