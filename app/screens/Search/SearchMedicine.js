1b4111bcb9296b0219a30ecdae48cc39f5552581
yeyoung <yangyeyoung13@gmail.com>
2025-02-21 05:22:56 +0900

    feat: 약 검색 페이지 - 인기 검색어 UI 수정

--- app/screens/Search/SearchMedicine.js
+++ app/screens/Search/SearchMedicine.js
@@ -5,6 +5,8 @@ import { themes, pointColor } from './../../styles';
 
 import Delete from './../../../assets/icons/delete.svg';
 import Chevron from './../../../assets/icons/header/chevron.svg';
+import RankUp from './../../../assets/icons/ranking_up.svg';
+import RankDown from './../../../assets/icons/ranking_down.svg';
 import SearchBar from './../../components/SearchBar';
 
 const Container = styled(SafeAreaView)`
@@ -24,20 +26,25 @@ const ChevronIconButton = styled(TouchableOpacity)`
   margin-right: 12px;
 `;
 
-const RecentSearchesContainer = styled.View`
+const SearchesContainer = styled.View`
   margin-top: 25px;
 `;
 
-const RecentSearchHeader = styled.View`
+const RecentSearchesContainer = styled.View`
+`;
+
+const PopularSearchContainer = styled.View`
+`;
+
+const SearchSectionHeader = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
   padding-left: 25px;
   padding-right: 25px;
 `;
 
-const RecentSearchTitle = styled.Text`
+const SearchTitle = styled.Text`
   font-size: 16px;
   font-family: 'Pretendard-Semibold';
   color: ${themes.light.textColor.textPrimary};
@@ -53,16 +60,25 @@ const ClearAllText = styled.Text`
 
 const NoRecentSearchesText = styled.Text`
   font-size: 14px;
-  color: #0004;
+  font-family: 'Pretendard-Semibold';
+  color: ${themes.light.textColor.Primary30};
   text-align: center;
-  margin-top: 60px;
+  margin-top: 50px;
+  margin-bottom: 50px;
 `;
 
-const ScrollContainer = styled.ScrollView`
+const RecentSearchListContainer = styled.ScrollView`
   padding-left: 20px;
+  padding-top: 20px;
+  padding-bottom: 40px;
 `;
 
-const SearchItemButton = styled(TouchableOpacity)`
+const PopularSearchListContainer = styled.View`
+  margin-top: 20px;
+  gap: 18px;
+`;
+
+const RecentSearchItemButton = styled(TouchableOpacity)`
   flex-direction: row;
   align-items: center;
   border: 1px;
@@ -72,7 +88,7 @@ const SearchItemButton = styled(TouchableOpacity)`
   margin-right: 10px;
 `;
 
-const SearchItemText = styled.Text`
+const RecentSearchItemText = styled.Text`
   font-size: 14px;
   color: ${themes.light.textColor.textPrimary};
 `;
@@ -81,9 +97,65 @@ const DeleteIconButton = styled(TouchableOpacity)`
   margin-left: 10px;
 `;
 
+const PopularSearchItemButton = styled(TouchableOpacity)`
+  flex-direction: row;
+  align-items: center;
+  padding: 0 25px 0 25px;
+`;
+
+const RankingText = styled.Text`
+  font-size: 14px;
+  font-family: 'Pretendard-Bold';
+  font-weight: bold;
+  color: ${pointColor.pointPrimary};
+  margin-right: 20px;
+`;
+
+const PopularSearchText = styled.Text`
+  font-size: 14px;
+  font-family: 'Pretendard-Medium';
+  color: ${themes.light.textColor.textPrimary};
+  flex: 1;
+`;
+
+const IconContainer = styled.View`
+  width: 9.14px;
+  height: 17px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const RankingStayText = styled.Text`
+  font-size: 14px;
+  text-align: center;
+  color: ${themes.light.textColor.Primary30};
+`;
+
+const UpdateDateContainer = styled.View`
+  font-family: 'Pretendard-Medium';
+  align-self: flex-end;
+  margin-top: 10px;
+`;
+
+const UpdateDateText = styled.Text`
+  font-size: 11px;
+  font-family: 'Pretendard-Regular';
+  margin-right: 20px;
+  color: ${themes.light.textColor.Primary30};
+`;
+
 const SearchMedicineScreen = ({ navigation, route }) => {
     const [searchQuery, setSearchQuery] = useState('');
     const [recentSearches, setRecentSearches] = useState([]);
+
+    // 인기 검색어 (임시)데이터
+    const popularSearches = [
+      { rank: 1, term: '소화제', rankChange: 'up' },
+      { rank: 2, term: '베스타제당의정', rankChange: 'stay' },
+      { rank: 3, term: '해열제', rankChange: 'down' },
+      { rank: 4, term: '타이레놀', rankChange: 'up' },
+      { rank: 5, term: '제산제', rankChange: 'stay' },
+    ];
 
     const handleSearch = () => {
       if (searchQuery.trim() !== '') {
@@ -105,12 +177,34 @@ const SearchMedicineScreen = ({ navigation, route }) => {
       });
     };
 
+    const handlePopularSearchClick = (term) => {
+      const updatedSearches = [term, ...recentSearches.filter(item => item !== term)];
+      setRecentSearches(updatedSearches);
+      navigation.navigate('SearchMedicineResults', { 
+          searchQuery: term,
+          recentSearches: updatedSearches 
+      });
+    };
+
     const handleDeleteSearch = (query) => {
         setRecentSearches(recentSearches.filter(item => item !== query));
     };
 
     const handleClearAll = () => {
         setRecentSearches([]);
+    };
+
+    const getRankChangeIcon = (rankChange) => {
+      switch (rankChange) {
+          case 'up':
+              return <RankUp width={9.14} height={17}/>;
+          case 'down':
+              return <RankDown width={9.14} height={17} />;
+          case 'stay':
+              return <RankingStayText>-</RankingStayText>;
+          default:
+              return null;
+      }
     };
 
     return (
@@ -121,36 +215,58 @@ const SearchMedicineScreen = ({ navigation, route }) => {
             </ChevronIconButton>
             <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} onSearch={handleSearch} />
         </ChevronAndSearchContainer>
-
-        <RecentSearchesContainer>
-            <RecentSearchHeader>
-                <RecentSearchTitle>최근 검색어</RecentSearchTitle>
-                <ClearAllButton onPress={handleClearAll}>
-                    <ClearAllText>전체 삭제</ClearAllText>
-                </ClearAllButton>
-            </RecentSearchHeader>
-            
-            {recentSearches.length > 0 ? (
-                <ScrollContainer
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                >
-                    {recentSearches.map((item, index) => (
-                        <SearchItemButton key={index} onPress={() => handleRecentSearchClick(item)}>
-                            <SearchItemText>{item}</SearchItemText>
-                            <DeleteIconButton onPress={(e) => {
-                                e.stopPropagation();
-                                handleDeleteSearch(item);
-                            }}>
-                                <Delete height={10} width={10}/>
-                            </DeleteIconButton>
-                        </SearchItemButton>
-                    ))}
-                </ScrollContainer>
-            ) : (
-                <NoRecentSearchesText>검색 기록이 없습니다.</NoRecentSearchesText>
-            )}
-        </RecentSearchesContainer>
+        <SearchesContainer>
+          <RecentSearchesContainer>
+              <SearchSectionHeader>
+                  <SearchTitle>최근 검색어</SearchTitle>
+                  <ClearAllButton onPress={handleClearAll}>
+                      <ClearAllText>전체 삭제</ClearAllText>
+                  </ClearAllButton>
+              </SearchSectionHeader>      
+              {recentSearches.length > 0 ? (
+                  <RecentSearchListContainer
+                      horizontal
+                      showsHorizontalScrollIndicator={false}
+                  >
+                      {recentSearches.map((item, index) => (
+                          <RecentSearchItemButton key={index} onPress={() => handleRecentSearchClick(item)}>
+                              <RecentSearchItemText>{item}</RecentSearchItemText>
+                              <DeleteIconButton onPress={(e) => {
+                                  e.stopPropagation();
+                                  handleDeleteSearch(item);
+                              }}>
+                                  <Delete height={10} width={10}/>
+                              </DeleteIconButton>
+                          </RecentSearchItemButton>
+                      ))}
+                  </RecentSearchListContainer>
+              ) : (
+                  <NoRecentSearchesText>검색 기록이 없습니다.</NoRecentSearchesText>
+              )}
+          </RecentSearchesContainer>
+          <PopularSearchContainer>
+            <SearchSectionHeader>
+              <SearchTitle>인기 검색어</SearchTitle>
+            </SearchSectionHeader>
+            <PopularSearchListContainer>
+              {popularSearches.map((item) => (
+                  <PopularSearchItemButton 
+                      key={item.rank} 
+                      onPress={() => handlePopularSearchClick(item.term)}
+                  >
+                      <RankingText>{item.rank}</RankingText>
+                      <PopularSearchText>{item.term}</PopularSearchText>
+                      <IconContainer>
+                        {getRankChangeIcon(item.rankChange)}
+                      </IconContainer>
+                  </PopularSearchItemButton>
+              ))}
+            </PopularSearchListContainer>
+            <UpdateDateContainer>
+              <UpdateDateText>업데이트 2025-02-13</UpdateDateText>
+            </UpdateDateContainer>
+          </PopularSearchContainer>
+        </SearchesContainer>
       </Container>
     );
 };
