77fc84e3cb98fb1d8edd8423bdb096694462acbd
yeyoung <yangyeyoung13@gmail.com>
2025-02-22 02:23:34 +0900

    feat: 컴포넌트 추가 및 수정
    Footer 컴포넌트 추가
    
    검색바 컴포넌트 수정

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import {TouchableOpacity} from 'react-native';
-
 import {themes} from './../styles';
 import {OtherIcons} from './../../assets/icons';
 
@@ -11,34 +10,35 @@ const SearchBarContainer = styled.View`
   height: 44px;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
   border-radius: 10px;
   background-color: ${({theme}) => themes.light.boxColor.inputSecondary};
   flex: 1;
+  padding: 13px 20px 13px 15px;
 `;
 
 const SearchInput = styled.TextInput`
-  font-size: 14px;
-  margin-left: 10px;
+  font-size: 15px;
+  font-family: 'Pretendard-SemiBold';
   flex: 1;
 `;
 
 const SearchButton = styled(TouchableOpacity)`
-  width: 15px;
-  height: 15px;
-  margin-left: 15px;
+  justify-content: center;
+  align-items: center;
 `;
 
 const SearchBar = ({searchQuery, setSearchQuery, onSearch}) => {
   return (
     <SearchBarContainer>
-      <SearchButton onPress={onSearch}>
-        <SearchGeneralIcon width={15} height={15} />
-      </SearchButton>
       <SearchInput
         placeholder="약 이름, 증상을 입력하세요"
         value={searchQuery}
         onChangeText={setSearchQuery}
       />
+      <SearchButton onPress={onSearch}>
+        <SearchGeneralIcon width={17.5} height={17.5} />
+      </SearchButton>
     </SearchBarContainer>
   );
 };
--- app/components/index.js
+++ app/components/index.js
@@ -1,4 +1,7 @@
 import Button from './Button';
 import ProgressBar from './ProgressBar';
+import SearchBar from './SearchBar';
+import Footer from './Footer';
 
-export { Button, ProgressBar };
+
+export { Button, ProgressBar, SearchBar, Footer };
