9bc3a7f83a03fa11dba0a07e2cd434eb721fbbdb
hjuump <butter0488@gmail.com>
2025-02-21 10:20:53 +0900

    feat: icons.js에 아이콘 변수 추가

--- assets/icons.js
+++ assets/icons.js
@@ -1,11 +1,100 @@
+// 네비게이션 아이콘
 import HomeIcon from './icons/nav/home.svg';
 import SearchIcon from './icons/nav/search.svg';
 import RoutineIcon from './icons/nav/routine.svg';
 import MyIcon from './icons/nav/my.svg';
 
+// 카메라 아이콘
+import CameraIcon from './icons/camera/camera.svg';
+import CameraSwitchIcon from './icons/camera/cameraSwitch.svg';
+import FlashIcon from './icons/camera/flash.svg';
+import TipIcon from './icons/camera/tip.svg';
+
+// 헤더 아이콘
+import ChevronIcon from './icons/header/chevron.svg';
+import CogIcon from './icons/header/cog.svg';
+import NotificationIcon from './icons/header/notification.svg';
+import TrashIcon from './icons/header/trash.svg';
+
+// 루틴 아이콘
+import CheckOffIcon from './icons/routine/checkOff.svg';
+import CheckOnIcon from './icons/routine/checkOn.svg';
+import HospitalIcon from './icons/routine/hospital.svg';
+import MedicineIcon from './icons/routine/medicine.svg';
+
+// 설정 아이콘
+import AnnouncementIcon from './icons/settings/announcement.svg';
+import AppVersionIcon from './icons/settings/app_version.svg';
+import DeleteIcon from './icons/settings/delete.svg';
+import FAQIcon from './icons/settings/faq.svg';
+import FavoritesIcon from './icons/settings/favorites.svg';
+import FeedbackIcon from './icons/settings/feedback.svg';
+import LogoutIcon from './icons/settings/logout.svg';
+import NotificationsIcon from './icons/settings/notifications.svg';
+import ProfileSettingsIcon from './icons/settings/profile_settings.svg';
+import TextSizeIcon from './icons/settings/text_size.svg';
+
+// 기타 아이콘
+import ChatIcon from './icons/chat.svg';
+import DeleteCircleIcon from './icons/delete_circle.svg';
+import HeartOffIcon from './icons/heartOff.svg';
+import HeartOnIcon from './icons/heartOn.svg';
+import KarteIcon from './icons/karte.svg';
+import RankingDownIcon from './icons/ranking_down.svg';
+import RankingUpIcon from './icons/ranking_up.svg';
+import ReturnIcon from './icons/return.svg';
+import SearchGeneralIcon from './icons/search.svg';
+
+// 전체 아이콘 객체로 정리
 export const TabIcons = {
   home: HomeIcon,
   search: SearchIcon,
   routine: RoutineIcon,
   my: MyIcon,
+};
+
+export const CameraIcons = {
+  camera: CameraIcon,
+  cameraSwitch: CameraSwitchIcon,
+  flash: FlashIcon,
+  tip: TipIcon,
+};
+
+export const HeaderIcons = {
+  chevron: ChevronIcon,
+  cog: CogIcon,
+  notification: NotificationIcon,
+  trash: TrashIcon,
+};
+
+export const RoutineIcons = {
+  checkOff: CheckOffIcon,
+  checkOn: CheckOnIcon,
+  hospital: HospitalIcon,
+  medicine: MedicineIcon,
+};
+
+export const SettingsIcons = {
+  announcement: AnnouncementIcon,
+  appVersion: AppVersionIcon,
+  delete: DeleteIcon,
+  faq: FAQIcon,
+  favorites: FavoritesIcon,
+  feedback: FeedbackIcon,
+  logout: LogoutIcon,
+  notifications: NotificationsIcon,
+  profileSettings: ProfileSettingsIcon,
+  textSize: TextSizeIcon,
+};
+
+export const OtherIcons = {
+  chat: ChatIcon,
+  deleteCircle: DeleteCircleIcon,
+  heartOff: HeartOffIcon,
+  heartOn: HeartOnIcon,
+  karte: KarteIcon,
+  rankingDown: RankingDownIcon,
+  rankingUp: RankingUpIcon,
+  return: ReturnIcon,
+  search: SearchGeneralIcon,
 };
