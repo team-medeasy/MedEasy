dea083c4387fc3e5471924335f7b0c8e0b85d831
kimgazii <gy0320ya@naver.com>
2025-02-23 01:33:46 +0900

    feat: MyPage 목록 및 Footer 추가 프로필 사진 버튼 추가 필요

--- app/screens/Navigation/MyPage.js
+++ app/screens/Navigation/MyPage.js
@@ -1,8 +1,11 @@
-import { Header } from '@react-navigation/stack';
 import React from 'react';
 import styled from 'styled-components/native';
 import { ScrollView } from 'react-native';
 import { pointColor, themes } from './../../styles';
+import KarteIcon from './../../../assets/icons/karte.svg';
+import LogoIcon from './../../../assets/icons/logo/logo.svg';
+import { SettingsIcons } from './../../../assets/icons';
+import { Footer } from '../../components';
 
 const MyPage = () => {
   return (
@@ -18,15 +21,66 @@ const MyPage = () => {
           </TextContainer>
           <InfoContainer>
             <DaysSinceMedication>
-              <InfoText>약 챙겨먹은지 5일째</InfoText>
+              <WithMedeasy>메디지와 함께</WithMedeasy>
+              <InfoText>약 챙겨먹은지 </InfoText>
+              <InfoNum>32일째</InfoNum>
+              <IconWrapper><KarteIcon /></IconWrapper>
             </DaysSinceMedication>
             <MedicationCount>
-              <InfoText>복용중인 약 3개</InfoText>
+              <WithMedeasy>메디지와 함께</WithMedeasy>
+              <InfoText>복용중인 약 </InfoText>
+              <InfoNum>5개</InfoNum>
+              <IconWrapper><LogoIcon /></IconWrapper>
             </MedicationCount>
           </InfoContainer>
         </ProfileContainer>
-        <ProfileSettings />
-        <Settings />
+        <ProfileSettings>
+          <SettingItem>
+            <SettingsIcons.profileSettings width={20} height={20} />
+            <SettingText>프로필 설정</SettingText>
+          </SettingItem>
+          <SettingItem>
+            <SettingsIcons.notifications width={20} height={20} />
+            <SettingText>알림 설정</SettingText>
+          </SettingItem>
+          <SettingItem>
+            <SettingsIcons.textSize width={20} height={20} />
+            <SettingText>글자 크기 설정</SettingText>
+          </SettingItem>
+          <SettingItem>
+            <SettingsIcons.favorites width={20} height={20} />
+            <SettingText>관심 목록</SettingText>
+          </SettingItem>
+        </ProfileSettings>
+        <Settings>
+          <SettingItem>
+            <SettingsIcons.announcement width={20} height={20} />
+            <SettingText>공지사항</SettingText>
+          </SettingItem>
+          <SettingItem>
+            <SettingsIcons.feedback width={20} height={20} />
+            <SettingText>의견 남기기</SettingText>
+          </SettingItem>
+          <SettingItem>
+            <SettingsIcons.faq width={20} height={20} />
+            <SettingText>자주 하는 질문</SettingText>
+          </SettingItem>
+          <SettingItem>
+            <SettingsIcons.appVersion width={20} height={20} />
+            <SettingText>앱 버전</SettingText>
+          </SettingItem>
+        </Settings>
+        <AccountSetting>
+          <SettingItem>
+            <SettingsIcons.logout width={20} height={20} />
+            <SettingText>로그아웃</SettingText>
+          </SettingItem>
+          <SettingItem>
+            <SettingsIcons.trashcan width={20} height={20} />
+            <SettingText>계정 삭제</SettingText>
+          </SettingItem>
+        </AccountSetting>
+        <Footer />
       </ScrollContent>
     </Container>
   );
@@ -58,6 +112,7 @@ const ScrollContent = styled.ScrollView`
 const ProfileContainer = styled.View`
   justify-content: center;
   background-color: ${themes.light.boxColor.buttonPrimary};
+  padding-bottom: 20px;
 `;
 const TextContainer = styled.View`
   justify-content: center;
@@ -88,29 +143,75 @@ const InfoContainer = styled.View`
 const DaysSinceMedication = styled.View`
   background-color: ${pointColor.pointPrimary};
   padding: 15px;
+  width: 49%;
+  aspectRatio: 1;
   border-radius: 10px; 
 `;
 
 const MedicationCount = styled.View`
   background-color: ${pointColor.pointPrimaryDark};
   padding: 15px;
+  width: 49%;
+  aspectRatio: 1;
   border-radius: 10px;
 `;
 
+const IconWrapper = styled.View`
+  position: absolute;
+  transform: rotate(8deg);
+  overflow: hidden;
+  bottom: -10px;
+  right: 20px;
+`;
+
+const WithMedeasy = styled.Text`
+  font-size: 12px;
+  font-family: 'Pretendard-Regular';
+  color: ${themes.light.textColor.buttonText};
+  padding-bottom: 10px;
+`;
+
 const InfoText = styled.Text`
-  font-size: 16px;
+  font-size: 18px;
+  font-family: 'KimjungchulGothic-Bold';
+  color: ${themes.light.textColor.buttonText70};
+`;
+
+const InfoNum = styled.Text`
+  font-size: 18px;
   font-family: 'KimjungchulGothic-Bold';
   color: ${themes.light.textColor.buttonText};
 `;
 
 const ProfileSettings = styled.View`
+  background-color: ${themes.light.bgColor.bgPrimary};
   justify-content: center;
-  border-bottom-width: 10px;
-  border-bottom-color: ${themes.light.borderColor.borderPrimary};
+  margin-bottom: 10px;
 `;
 
 const Settings = styled.View`
+  background-color: ${themes.light.bgColor.bgPrimary};
   justify-content: center;
+  margin-bottom: 10px;
+`;
+
+const AccountSetting = styled.View`
+  background-color: ${themes.light.bgColor.bgPrimary};
+  justify-content: center;
+`;
+
+const SettingItem = styled.TouchableOpacity`
+  flex-direction: row;
+  padding: 16px 25px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${themes.light.borderColor.borderPrimary};
+`;
+
+const SettingText = styled.Text`
+  font-size: 16px;
+  font-family: 'Pretendard-Regular';
+  color: ${themes.light.textColor.textPrimary};
+  margin-left: 20px;
 `;
 
 export default MyPage;
