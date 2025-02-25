bffd9e66c123d175c6d65faade57fc17a9434362
yeyoung <yangyeyoung13@gmail.com>
2025-02-26 02:47:34 +0900

    feat: Header 컴포넌트 분리 및 수정 - border-bottom 추가 - 제목 텍스트 전달하여 사용

---
+++ app/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { HeaderIcons } from '../../assets/icons';
+import { themes } from '../styles';
+import FontSizes from '../../assets/fonts/fontSizes';
+
+const Header = ({ children, onBackPress }) => {
+    const navigation = useNavigation();
+    const handleBackPress = onBackPress || (() => navigation.goBack());
+    
+    return (
+      <HeaderContainer>
+        <BackAndTitleContainer>
+          <TouchableOpacity onPress={handleBackPress}>
+            <HeaderIcons.chevron
+              width={17}
+              height={17}
+              style={{ color: themes.light.textColor.textPrimary }}
+            />
+          </TouchableOpacity>
+          <Title>{children}</Title>
+          <View width={17} height={17} />
+        </BackAndTitleContainer>
+      </HeaderContainer>
+    );
+  };
+
+const HeaderContainer = styled.View`
+  height: 108px;
+  justify-content: flex-end;
+  padding-bottom: 10px;
+  background-color: ${themes.light.bgColor.bgPrimary};
+  border-bottom-width: 1;
+  border-bottom-color: ${themes.light.borderColor.borderPrimary};
+`;
+
+const BackAndTitleContainer = styled.View`
+  flex-direction: row;
+  padding: 0 15px;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  flex: 1;
+  text-align: center;
+  font-family: 'Pretendard-SemiBold';
+  font-size: ${FontSizes.body.default};
+  color: ${themes.light.textColor.textPrimary};
+`;
+
+export {Header} ;
--- app/components/index.js
+++ app/components/index.js
@@ -1,5 +1,6 @@
 export { Button, BackAndNextButtons } from './Button';
 export { ProgressBar } from './ProgressBar';
 export { SearchBar } from './SearchBar';
+export { Header } from './Header';
 export { Footer } from './Footer';
 export { Tag } from './Tag';
