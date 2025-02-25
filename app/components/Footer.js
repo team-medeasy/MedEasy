dabf802bf39f1a0fe25b741c6820792adc1dda48
yeyoung <yangyeyoung13@gmail.com>
2025-02-25 19:28:12 +0900

    style: 밑줄 스타일 변경

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -11,15 +11,18 @@ const {
 const Footer = () => {
   return (
     <FooterContainer>
-      <FooterText>
+      <FooterText      
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: themes.light.borderColor.borderPrimary  
+        }}>
         모든 의약품 정보는 참고용으로 제공되며, 정확한 복용법은 의사 및 약사와 상담하시기 바랍니다.{'\n'}
         위 정보는 식품의약품안전처 오픈API(의약품 낱알식별 정보, 의약품개요정보)에서 제공하는 정보입니다.
       </FooterText>
 
-      <Divider />
-
       <FooterText>
-        관련 문의: https://github.com/team-medeasy{'\n'}© 2025 MedEasy.
+        관련 문의: https://github.com/team-medeasy{'\n'}
+        © 2025 MedEasy.
       </FooterText>
 
       <FooterLogoContainer>
@@ -41,12 +44,6 @@ const FooterText = styled.Text`
   color: ${themes.light.textColor.Primary30};
   padding: 20px 0;
   line-height: 20px;
-`;
-
-const Divider = styled.View`
-  width: 100%;
-  height: 1px;
-  background-color: ${themes.light.textColor.Primary20};
 `;
 
 const FooterLogoContainer = styled.View`
