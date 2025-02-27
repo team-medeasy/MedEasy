ed31046705be0f808be4d9e3ca12e76392efcfcc
hjuump <butter0488@gmail.com>
2025-02-27 10:53:04 +0900

    feat: 채팅 화면 메시지 버블, 텍스트 인풋 컴포넌트 분리

--- app/screens/Chat/Chat.js
+++ app/screens/Chat/Chat.js
@@ -3,15 +3,9 @@ import styled from 'styled-components/native';
 import {FlatList} from 'react-native';
 import ChatInfoModal from './ChatInfoModal';
 import {Header} from './../../components';
-import {ChatIcons} from '../../../assets/icons';
 import {themes} from '../../styles';
-
-const {
-  add: AddIcon,
-  mike: MikeIcon,
-  robot: RobotIcon,
-  send: SendIcon,
-} = ChatIcons;
+import MessageBubble from '../../components/Chat/MessageBubble';
+import MessageInput from '../../components/Chat/MessageInput';
 
 const Chat = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -60,43 +54,7 @@ const Chat = () => {
   };
 
   const renderMessage = ({item}) => {
-    if (item.type === 'bot') {
-      return (
-        <BotMessageContainer>
-          <RobotIconContainer>
-            <RobotIcon
-              height={30}
-              width={30}
-              style={{color: themes.light.textColor.buttonText}}
-            />
-          </RobotIconContainer>
-          <BotMessageBubble>
-            <BotText>{item.text}</BotText>
-            {item.options && (
-              <BotOptions>
-                {item.options.map((option, index) => (
-                  <OptionButton
-                    key={index}
-                    isLast={index === item.options.length - 1}>
-                    <OptionText>{option}</OptionText>
-                  </OptionButton>
-                ))}
-              </BotOptions>
-            )}
-          </BotMessageBubble>
-          <MessageTime>{item.time}</MessageTime>
-        </BotMessageContainer>
-      );
-    } else {
-      return (
-        <UserMessageContainer>
-          <MessageTime>{item.time}</MessageTime>
-          <UserMessageBubble>
-            <UserText>{item.text}</UserText>
-          </UserMessageBubble>
-        </UserMessageContainer>
-      );
-    }
+    return <MessageBubble item={item} />;
   };
 
   return (
@@ -117,195 +75,19 @@ const Chat = () => {
         contentContainerStyle={{padding: 16}}
       />
 
-      {/* 메시지 입력창 */}
-      <InputContainer>
-        {/* 추가 아이콘 컨테이너 */}
-        <AddIconContainer>
-          <AddIcon
-            width={18}
-            height={18}
-            style={{color: themes.light.textColor.Primary30}}
-          />
-        </AddIconContainer>
-
-        {/* 입력 필드 및 전송 버튼 컨테이너 */}
-        <TextInputContainer>
-          <Input
-            placeholder="무엇이든 물어보세요!"
-            value={inputText}
-            onChangeText={setInputText}
-            multiline={true}
-          />
-          <SendButton onPress={sendMessage}>
-            <SendIcon
-              width={18}
-              height={18}
-              style={{color: themes.light.textColor.Primary30}}
-            />
-          </SendButton>
-        </TextInputContainer>
-
-        {/* 마이크 아이콘 컨테이너 */}
-        <MikeIconContainer>
-          <MikeIcon
-            width={17}
-            height={20}
-            style={{color: themes.light.textColor.Primary}}
-          />
-        </MikeIconContainer>
-      </InputContainer>
+      {/* 메시지 입력 컴포넌트 */}
+      <MessageInput
+        inputText={inputText}
+        setInputText={setInputText}
+        sendMessage={sendMessage}
+      />
     </Container>
   );
 };
 
-// 스타일 정의
 const Container = styled.View`
   flex: 1;
   background-color: ${themes.light.bgColor.bgSecondary};
-`;
-
-const BotMessageContainer = styled.View`
-  flex-direction: row;
-  align-items: flex-start;
-  margin-bottom: 20px;
-  /* iOS 그림자 */
-  shadow-color: black;
-  shadow-offset: 5px 5px;
-  shadow-opacity: 0.07;
-  shadow-radius: 30px;
-  /* Android 그림자 */
-  elevation: 5;
-`;
-
-const RobotIconContainer = styled.View`
-  background-color: ${themes.light.pointColor.Primary};
-  width: 40px;
-  height: 40px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 20px;
-  margin-right: 10px;
-`;
-
-const BotMessageBubble = styled.View`
-  background-color: ${themes.light.bgColor.bgPrimary};
-  padding: 15px 20px;
-  border-radius: 20px;
-  border-top-left-radius: 3px;
-  max-width: 65%;
-`;
-
-const BotText = styled.Text`
-  font-size: 15px;
-  line-height: 24px;
-  color: ${themes.light.textColor.Primary};
-  font-family: 'Pretendard-Medium';
-`;
-
-const BotOptions = styled.View`
-  margin-top: 15px;
-  margin-bottom: 5px;
-  background-color: ${themes.light.pointColor.Primary10};
-  border-radius: 12px;
-`;
-
-const OptionButton = styled.TouchableOpacity`
-  padding: 10px;
-  margin: 0px 10px;
-  justify-content: center;
-  align-items: center;
-  border-bottom-width: ${({isLast}) => (isLast ? '0px' : '1px')};
-  border-bottom-color: ${themes.light.borderColor.borderPrimary};
-`;
-
-const OptionText = styled.Text`
-  color: ${themes.light.pointColor.Primary};
-  font-size: 15px;
-  font-family: 'Pretendard-Semibold';
-`;
-
-const UserMessageContainer = styled.View`
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: flex-end;
-  margin-bottom: 20px;
-`;
-
-const UserText = styled.Text`
-  font-size: 15px;
-  line-height: 24px;
-  color: ${themes.light.textColor.buttonText};
-  font-family: 'Pretendard-SemiBold';
-`;
-
-const MessageTime = styled.Text`
-  font-size: 12px;
-  margin: 0px 10px;
-  align-self: flex-end;
-  color: ${themes.light.textColor.Primary30};
-  font-family: 'Pretendard-Medium';
-`;
-
-const UserMessageBubble = styled.View`
-  background-color: ${themes.light.pointColor.Primary};
-  padding: 15px 20px;
-  border-radius: 20px;
-  border-top-right-radius: 3px;
-  max-width: 70%;
-`;
-
-const InputContainer = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  padding: 10px 10px;
-  padding-bottom: 30px;
-  background-color: ${themes.light.bgColor.bgPrimary};
-`;
-
-const AddIconContainer = styled.View`
-  width: 40px;
-  height: 40px;
-  border-radius: 100%;
-  justify-content: center;
-  align-items: center;
-  background-color: ${themes.light.boxColor.inputSecondary};
-  flex-shrink: 0;
-`;
-
-const TextInputContainer = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  border-radius: 20px;
-  margin: 0px 10px;
-  min-height: 40px;
-  padding: 5px 0px;
-  background-color: ${themes.light.boxColor.inputSecondary};
-`;
-
-const MikeIconContainer = styled.View`
-  width: 40px;
-  height: 40px;
-  border-radius: 100%;
-  justify-content: center;
-  align-items: center;
-  background-color: ${themes.light.boxColor.inputSecondary};
-  flex-shrink: 0;
-`;
-
-const Input = styled.TextInput`
-  flex: 1;
-  padding-left: 15px;
-  padding-right: 10px;
-  font-size: 16px;
-  max-height: 100px;
-`;
-
-const SendButton = styled.TouchableOpacity`
-  padding-right: 15px;
 `;
 
 export default Chat;
