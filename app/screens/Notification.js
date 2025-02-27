13c21d69799b553c9405fb9519cc8abb56821d5f
hjuump <butter0488@gmail.com>
2025-02-27 11:56:41 +0900

    feat: 알림 화면 더미데이터 추가 및 스타일 수정

--- app/screens/Notification.js
+++ app/screens/Notification.js
@@ -3,26 +3,122 @@ import styled from 'styled-components/native';
 import {FlatList} from 'react-native';
 import {themes} from '../styles';
 import {Header} from '../components';
+import {RoutineIcons} from './../../assets/icons';
+
+const {medicine: MediIcon, hospital: HospitalIcon} = RoutineIcons;
 
 const Notification = () => {
   const notifications = [
-    {id: 1, title: '복용 알림', message: '오후 8시에 약을 복용하세요.'},
+    {
+      id: 1,
+      title: '아침 식사 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '3분 전',
+      type: 'medicine',
+    },
     {
       id: 2,
-      title: '진료 알림',
-      message: '내일 오전 10시에 병원 진료가 있습니다.',
+      title: '취침 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '6시간 전',
+      type: 'medicine',
     },
     {
       id: 3,
-      title: '새로운 메시지',
-      message: 'AI 챗봇 메디씨로부터 새로운 메시지가 도착했습니다.',
+      title: '저녁 식사 전 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '9시간 전',
+      type: 'medicine',
+    },
+    {
+      id: 4,
+      title: '오늘은 병원 진료 예약이 있어요.',
+      message: '오전 10시 30분 한성대병원 외래진료',
+      time: '어제',
+      type: 'hospital',
+    },
+    {
+      id: 5,
+      title: '아침 식사 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '3분 전',
+      type: 'medicine',
+    },
+    {
+      id: 6,
+      title: '취침 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '6시간 전',
+      type: 'medicine',
+    },
+    {
+      id: 7,
+      title: '저녁 식사 전 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '9시간 전',
+      type: 'medicine',
+    },
+    {
+      id: 8,
+      title: '오늘은 병원 진료 예약이 있어요.',
+      message: '오전 10시 30분 한성대병원 외래진료',
+      time: '어제',
+      type: 'hospital',
+    },
+    {
+      id: 9,
+      title: '아침 식사 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '3분 전',
+      type: 'medicine',
+    },
+    {
+      id: 10,
+      title: '취침 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '6시간 전',
+      type: 'medicine',
+    },
+    {
+      id: 11,
+      title: '저녁 식사 전 전 복용해야 하는 약이 있어요.',
+      message: '파모시드정 20mg(내복), 슬로젠정(내복) 외 2건',
+      time: '9시간 전',
+      type: 'medicine',
+    },
+    {
+      id: 12,
+      title: '오늘은 병원 진료 예약이 있어요.',
+      message: '오전 10시 30분 한성대병원 외래진료',
+      time: '어제',
+      type: 'hospital',
     },
   ];
 
   const renderItem = ({item}) => (
     <NotificationItem>
-      <NotificationTitle>{item.title}</NotificationTitle>
-      <NotificationMessage>{item.message}</NotificationMessage>
+      <NotiContainer>
+        {item.type === 'medicine' ? (
+          <MediIcon
+            width={18}
+            height={18}
+            marginRight={15}
+            style={{color: themes.light.pointColor.Primary}}
+          />
+        ) : item.type === 'hospital' ? (
+          <HospitalIcon
+            width={18}
+            height={18}
+            marginRight={15}
+            style={{color: themes.light.pointColor.Secondary}}
+          />
+        ) : null}
+        <NotiTextContainer>
+          <NotificationTitle>{item.title}</NotificationTitle>
+          <NotificationMessage>{item.message}</NotificationMessage>
+        </NotiTextContainer>
+      </NotiContainer>
+      <NotiTime>{item.time}</NotiTime>
     </NotificationItem>
   );
 
@@ -33,7 +129,7 @@ const Notification = () => {
         data={notifications}
         renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
-        contentContainerStyle={{padding: 16}}
+        contentContainerStyle={{paddingBottom: 100}}
       />
     </Container>
   );
@@ -44,21 +140,34 @@ const Container = styled.View`
 `;
 
 const NotificationItem = styled.View`
-  padding: 16px;
-  border-bottom-width: 1px;
-  border-bottom-color: ${themes.light.borderColor.primary};
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
 `;
 
+const NotiContainer = styled.View`
+  flex-direction: row;
+`;
+
+const NotiTextContainer = styled.View``;
+
 const NotificationTitle = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
+  font-size: 15px;
   color: ${themes.light.textColor.textPrimary};
-  margin-bottom: 8px;
+  font-family: 'Pretendard-Bold';
+  margin-bottom: 10px;
 `;
 
 const NotificationMessage = styled.Text`
-  font-size: 16px;
-  color: ${themes.light.textColor.textSecondary};
+  font-size: 12px;
+  color: ${themes.light.textColor.Primary70};
+  font-family: 'Pretendard-Medium';
+`;
+
+const NotiTime = styled.Text`
+  font-size: 12px;
+  color: ${themes.light.textColor.Primary30};
+  font-family: 'Pretendard-SemiBold';
 `;
 
 export default Notification;
