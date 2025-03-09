e9298fa927b5e331d41f2d9573c130f2eb33191a
hjuump <butter0488@gmail.com>
2025-03-09 19:24:09 +0900

    feat: 로그인 화면 내용 추가

---
+++ app/screens/SignUp/SignIn.js
@@ -0,0 +1,117 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import styled from 'styled-components/native';
+import {themes, fonts} from './../../styles';
+import {ProgressBar, BackAndNextButtons} from './../../components';
+import {useSignUp} from '../../api/context/SignUpContext';
+
+const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${themes.light.bgColor.bgPrimary};
+`;
+
+const Container1 = styled.View`
+  justify-content: center;
+  margin-top: 78px;
+  margin-left: 30px;
+`;
+
+const Container2 = styled.View`
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-top: 37px;
+`;
+
+const InputContainer = styled.View`
+  width: 100%;
+  margin-top: ${props => props.marginTop || '0px'};
+  margin-bottom: ${props => props.marginBottom || '0px'};
+`;
+
+const BtnContainer = styled.View`
+  margin-top: auto;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+const TextInput = styled.TextInput`
+  height: 60px;
+  border-radius: 10px;
+  background-color: ${themes.light.boxColor.inputPrimary};
+  padding: 20px;
+  font-size: 16px;
+`;
+
+const TxtLabel = styled.Text`
+  height: 60px;
+  border-radius: 10px;
+  background-color: ${themes.light.boxColor.inputPrimary};
+  padding: 20px;
+  font-size: 16px;
+`;
+
+const SignInScreen = ({navigation, route}) => {
+  const {updateSignUpData} = useSignUp();
+  const [email, setEmail] = useState('');
+  const progress = '50%';
+
+  const handleNext = () => {
+    if (email) {
+      updateSignUpData({email});
+      // 다음 페이지로 이동
+      navigation.navigate('SignUpPassword');
+    } else {
+      alert('이메일을 입력하세요.');
+    }
+  };
+
+  return (
+    <Container>
+      <Container1>
+        <Text
+          style={{
+            fontFamily: fonts.title.fontFamily,
+            fontSize: fonts.title.fontSize,
+          }}>
+          안녕하세요, 메디지입니다 👋
+        </Text>
+        <Text
+          style={{
+            fontFamily: 'Pretendard-Medium',
+            fontSize: 16,
+            marginTop: 7,
+            color: themes.light.textColor.Primary50,
+          }}>
+          로그인 후 다양한 서비스를 이용해 보세요!
+        </Text>
+      </Container1>
+
+      <Container2>
+        <InputContainer marginBottom="5px">
+          <TextInput
+            placeholder="이메일 입력"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </InputContainer>
+        <InputContainer marginBottom="5px">
+          <TextInput
+            placeholder="이메일 입력"
+            value={email}
+            onChangeText={setEmail}
+            secureTextEntry={true}
+          />
+        </InputContainer>
+      </Container2>
+
+      <BtnContainer>
+        <BackAndNextButtons
+          onPressPrev={() => navigation.goBack()}
+          onPressNext={handleNext}
+        />
+      </BtnContainer>
+    </Container>
+  );
+};
+
+export default SignInScreen;
