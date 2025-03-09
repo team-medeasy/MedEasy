d55a37867537d611f084c40b1428dbebce2496cf
hjuump <butter0488@gmail.com>
2025-03-09 19:24:55 +0900

    feat: 로그인 프로바이더 생성

---
+++ app/api/context/SignUpContext.js
@@ -0,0 +1,36 @@
+import React, {createContext, useContext, useState} from 'react';
+
+const SignUpContext = createContext();
+
+export const SignUpProvider = ({children}) => {
+  const [signUpData, setSignUpData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    birthday: '',
+    gender: '',
+  });
+
+  const updateSignUpData = newData => {
+    console.log('Updating SignUp Data:', newData);
+    setSignUpData(prev => ({...prev, ...newData}));
+  };
+
+  console.log('Current SignUp Data:', signUpData);
+
+  return (
+    <SignUpContext.Provider value={{signUpData, updateSignUpData}}>
+      {children}
+    </SignUpContext.Provider>
+  );
+};
+
+export const useSignUp = () => {
+  const context = useContext(SignUpContext);
+  if (!context) {
+    throw new Error('useSignUp must be used within a SignUpProvider');
+  }
+  console.log('SignUp Context:', context);
+  return context;
+};
