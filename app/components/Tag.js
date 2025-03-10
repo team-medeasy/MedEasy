e67d56f5a72dd8323b5118b918c8e1d7c3ce59d4
hjuump <butter0488@gmail.com>
2025-03-10 20:59:46 +0900

    fix: 로그인 시 토큰 전달 로직 수정

--- app/components/Tag.js
+++ app/components/Tag.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { themes } from '../styles';
+import {themes} from '../styles';
 
 const size = {
   small: {
@@ -38,46 +38,56 @@ const TagContainer = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  font-size: ${(props) => (props.sizeType === 'small' ? size.small.fontSize : size.large.fontSize)};
-  font-family: ${(props) => (props.sizeType === 'small' ? size.small.fontFamily : size.large.fontFamily)};
-  background-color: ${(props) => 
-    props.colorType ? 
-      color[props.colorType].bgColor : 
-      props.bgColor || themes.light.boxColor.tagDetailPrimary};
+  font-size: ${props =>
+    props.sizeType === 'small' ? size.small.fontSize : size.large.fontSize};
+  font-family: ${props =>
+    props.sizeType === 'small' ? size.small.fontFamily : size.large.fontFamily};
+  background-color: ${props =>
+    props.colorType
+      ? color[props.colorType].bgColor
+      : props.bgColor || themes.light.boxColor.tagDetailPrimary};
   border-radius: 5px;
-  padding: ${(props) => (props.sizeType === 'small' ? size.small.padding : size.large.padding)};
+  padding: ${props =>
+    props.sizeType === 'small' ? size.small.padding : size.large.padding};
   align-self: flex-start;
 `;
 
 const TagText = styled.Text`
-  color: ${(props) => 
-    props.colorType ? 
-      color[props.colorType].color : 
-      props.color || themes.light.textColor.buttonText};
-  font-size: ${(props) => (props.sizeType === 'small' ? size.small.fontSize : size.large.fontSize)};
-  font-family: ${(props) => (props.sizeType === 'small' ? size.small.fontFamily : size.large.fontFamily)};
+  color: ${props =>
+    props.colorType
+      ? color[props.colorType].color
+      : props.color || themes.light.textColor.buttonText};
+  font-size: ${props =>
+    props.sizeType === 'small' ? size.small.fontSize : size.large.fontSize};
+  font-family: ${props =>
+    props.sizeType === 'small' ? size.small.fontFamily : size.large.fontFamily};
 `;
 
-const Tag = ({ 
-  children, 
-  sizeType = 'large', 
-  colorType, 
-  bgColor, 
-  color, 
-  style, 
-  ...rest 
+const Tag = ({
+  children,
+  sizeType = 'large',
+  colorType,
+  bgColor,
+  color,
+  style,
+  ...rest
 }) => {
   return (
-    <TagContainer 
-      sizeType={sizeType} 
-      colorType={colorType} 
-      bgColor={bgColor} 
-      style={style} 
-      {...rest}
-    >
-      {typeof children === 'string' ? <TagText sizeType={sizeType} colorType={colorType}>{children}</TagText> : children}
+    <TagContainer
+      sizeType={sizeType}
+      colorType={colorType}
+      bgColor={bgColor}
+      style={style}
+      {...rest}>
+      {typeof children === 'string' ? (
+        <TagText sizeType={sizeType} colorType={colorType}>
+          {children}
+        </TagText>
+      ) : (
+        children
+      )}
     </TagContainer>
   );
 };
 
-export { Tag };
+export {Tag};
