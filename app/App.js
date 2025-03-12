b100ddc37c0a217fc7172f13fd0c6f20b798586c
yeyoung <yangyeyoung13@gmail.com>
2025-03-12 19:28:01 +0900

    refactor: DateTimePickerModal 컴포넌트 분리 및 적용 - 병원 일정 추가 페이지 날짜/시간 default > 현재 - 병원 일정 추가 페이지 스타일 수정

--- app/App.js
+++ app/App.js
@@ -100,6 +100,7 @@ const App = () => {
               <RootStack.Screen
                 name="AddMedicineRoutine"
                 component={AddMedicineRoutineScreen}
+                options={{presentation: 'modal'}}
               />
               <RootStack.Screen
                 name="AddHospitalVisit"
--- app/screens/Routine/AddHospitalVisit.js
+++ app/screens/Routine/AddHospitalVisit.js
@@ -1,29 +1,20 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
-import { TouchableOpacity, Modal, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { themes } from './../../styles';
-import { ModalHeader } from '../../components';
+import { ModalHeader, DateTimePickerModal } from '../../components';
 import { RoutineIcons } from '../../../assets/icons';
 import { Button } from '../../components/Button';
-import DateTimePicker from '@react-native-community/datetimepicker';
 import FontSizes from '../../../assets/fonts/fontSizes';
 
 const { hospital: HospitalIcon } = RoutineIcons;
 
 const AddHospitalVisit = () => {
+  const currentDate = new Date();
   const [hospitalName, setHospitalName] = useState('');
-  const [time, setTime] = useState('');
-  const [date, setDate] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [pickerMode, setPickerMode] = useState('date'); // 'date' 또는 'time'
   const [selectedDate, setSelectedDate] = useState(new Date());
-
-  const handleAdd = () => {
-    // 추가 로직 구현
-    console.log('병원 일정명:', hospitalName);
-    console.log('시간:', time);
-    console.log('날짜:', date);
-  };
 
   const formatDate = (date) => {
     if (!date) return '';
@@ -37,7 +28,16 @@ const AddHospitalVisit = () => {
     const ampm = hours >= 12 ? '오후' : '오전';
     const formattedHours = hours % 12 || 12;
     const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-    return `${ampm} ${formattedHours}:${formattedMinutes}`;
+    return `${ampm} ${formattedHours}시 ${formattedMinutes}분`;
+  };
+
+  const [time, setTime] = useState(formatTime(currentDate));
+  const [date, setDate] = useState(formatDate(currentDate));
+
+  const handleAdd = () => {
+    console.log('병원 일정명:', hospitalName);
+    console.log('시간:', time);
+    console.log('날짜:', date);
   };
 
   const onDateChange = (event, selectedDate) => {
@@ -70,7 +70,10 @@ const AddHospitalVisit = () => {
 
   return (
     <Container>
-      <ModalHeader>병원 진료 추가</ModalHeader>
+      <ModalHeader
+        showDelete={true}
+        onDeletePress={() => {}}
+      >병원 일정 추가</ModalHeader>
       <MainContainer>
         <InputContainer>
           <HospitalIcon
@@ -89,7 +92,7 @@ const AddHospitalVisit = () => {
           <PickerTitle>날짜</PickerTitle>
           <SchedulePicker onPress={openDatePicker}>
             <PickerPlaceHolder>
-              {date || '날짜를 선택해주세요.'}
+              {date}
             </PickerPlaceHolder>
           </SchedulePicker>
         </SchedulePickerContainer>
@@ -97,37 +100,21 @@ const AddHospitalVisit = () => {
           <PickerTitle>시각</PickerTitle>
           <SchedulePicker onPress={openTimePicker}>
             <PickerPlaceHolder>
-              {time || '시각을 선택해주세요.'}
+              {time}
             </PickerPlaceHolder>
           </SchedulePicker>
         </SchedulePickerContainer>
       </MainContainer>
 
-      <Modal
-        animationType="slide"
-        transparent={true}
+      <DateTimePickerModal
         visible={showModal}
-        onRequestClose={() => setShowModal(false)}>
-        <ModalContainer>
-          <ModalContent>
-            <TopBar />
-            <ModalTitle>{getModalTitleText()}</ModalTitle>
-            <View
-              style={{
-                margin: 30,
-              }}>
-              <DateTimePicker
-                value={selectedDate}
-                mode={pickerMode}
-                display="spinner"
-                onChange={onDateChange}
-                locale="ko"
-              />
-            </View>
-            <Button title="확인" onPress={handleConfirm} />
-          </ModalContent>
-        </ModalContainer>
-      </Modal>
+        onClose={() => setShowModal(false)}
+        onConfirm={handleConfirm}
+        mode={pickerMode}
+        date={selectedDate}
+        onChange={onDateChange}
+        title={getModalTitleText()}
+      />
 
       <BtnContainer>
         <Button
@@ -156,24 +143,24 @@ const InputContainer = styled.View`
 
 const TextInput = styled.TextInput`
   flex: 1;
-  font-size: 22px;
-  color: ${themes.light.textColor.Primary};
+  font-size: ${FontSizes.title.default};
+  color: ${themes.light.textColor.textPrimary};
   font-family: 'Pretendard-ExtraBold';
 `;
 
 const SchedulePickerContainer = styled.View``;
 
 const PickerTitle = styled.Text`
-  font-size: 18px;
+  font-size: ${FontSizes.heading.default};
   font-family: 'Pretendard-Bold';
   margin-bottom: 15px;
-  color: ${themes.light.textColor.Primary};
+  color: ${themes.light.textColor.textPrimary};
 `;
 
 const PickerPlaceHolder = styled.Text`
-  font-size: 15px;
+  font-size: ${FontSizes.body.default};
   font-family: 'Pretendard-SemiBold';
-  color: ${themes.light.textColor.Primary30};
+  color: ${themes.light.textColor.textPrimary};
 `;
 
 const SchedulePicker = styled(TouchableOpacity)`
@@ -188,38 +175,6 @@ const BtnContainer = styled.View`
   margin-top: auto;
   padding: 0px 20px;
   padding-bottom: 30px;
-`;
-
-// 새로운 모달 스타일 적용
-const ModalContainer = styled.View`
-  flex: 1;
-  background-color: ${themes.light.bgColor.modalBG};
-`;
-
-const ModalContent = styled.View`
-  width: 100%;
-  margin-top: auto;
-  background-color: ${themes.light.bgColor.bgPrimary};
-  border-top-left-radius: 40px;
-  border-top-right-radius: 40px;
-  align-items: center;
-  justify-content: space-between;
-  padding: 10px 20px 0px 20px;
-  padding-bottom: 40px;
-`;
-
-const TopBar = styled.View`
-  width: 40px;
-  height: 5px;
-  border-radius: 4px;
-  background-color: ${themes.light.boxColor.modalBar};
-  margin-bottom: 25px;
-`;
-
-const ModalTitle = styled.Text`
-  font-family: 'KimjungchulGothic-Bold';
-  font-size: ${FontSizes.title.default};
-  color: ${themes.light.textColor.textPrimary};
 `;
 
 export default AddHospitalVisit;
