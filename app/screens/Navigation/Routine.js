8053ca4c2869a1fc74dc9f88c265af458c7d1c5b
kimgazii <gy0320ya@naver.com>
2025-03-17 11:13:26 +0900

    refactor: 날짜 계산 함수 이동

--- app/screens/Navigation/Routine.js
+++ app/screens/Navigation/Routine.js
@@ -16,7 +16,6 @@ import {
   initialHospitalRoutines,
   weekDays,
   getWeekDays,
-  getRelativeDayText,
 } from '../../../assets/data/data';
 import FontSizes from '../../../assets/fonts/fontSizes';
 
@@ -85,6 +84,19 @@ const Routine = () => {
       }
     }, []),
   );
+
+  // 상대적 날짜 텍스트 생성 함수
+  const getRelativeDayText = (selectedDate, today) => {
+    const selectedDateObj = dayjs(`${selectedDate.year}-${selectedDate.month}-${selectedDate.date}`);
+    const todayObj = dayjs(`${today.year()}-${today.month() + 1}-${today.date()}`);
+    const diff = selectedDateObj.diff(todayObj, 'day');
+
+    if (diff === 0) return '오늘';
+    if (diff === -1) return '어제';
+    if (diff === 1) return '내일';
+    if (diff === 2) return '모레';
+    return diff < 0 ? `${Math.abs(diff)}일 전` : `${diff}일 후`;
+  };
 
   const [checkedItems, setCheckedItems] = useState({});
 
