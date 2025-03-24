5498d7f4ce76bfcda1cdc84c006b34804c660a1e
HONGMOEJI <moejihong@gmail.com>
2025-03-24 22:59:08 +0900

    fix: SignUpNameScreen 입력 UX 개선
    - 플레이스홀더 색상 수정
    - 빈 영역 터치 시 키보드 닫힘 기능 추가
    - 성 입력 후 이름 필드로 자동 포커스 이동 기능 구현
    - 키보드 완료 버튼 동작 최적화

--- app/screens/SignUp/SignUpName.js
+++ app/screens/SignUp/SignUpName.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, Keyboard, TouchableWithoutFeedback} from 'react-native';
 import styled from 'styled-components/native';
 import {themes, fonts} from './../../styles';
 import {ProgressBar, BackAndNextButtons} from './../../components';
@@ -43,6 +43,7 @@ const TextInput = styled.TextInput`
   background-color: ${themes.light.boxColor.inputPrimary};
   padding: 20px;
   font-size: 16px;
+  /* color: ${themes.light.textColor.Primary100 || '#000000'}; */
 `;
 
 const SignUpNameScreen = ({navigation}) => {
@@ -50,6 +51,7 @@ const SignUpNameScreen = ({navigation}) => {
   const [firstName, setFirstName] = useState(signUpData.firstName || '');
   const [lastName, setLastName] = useState(signUpData.lastName || '');
   const progress = '25%';
+  const secondInput = React.useRef();
 
   const handleNext = () => {
     if (firstName && lastName) {
@@ -62,49 +64,61 @@ const SignUpNameScreen = ({navigation}) => {
   };
 
   return (
-    <Container>
-      <ProgressBar progress={progress} />
-      <Container1>
-        <Text
-          style={{
-            fontFamily: fonts.title.fontFamily,
-            fontSize: fonts.title.fontSize,
-          }}>
-          안녕하세요, 메디지입니다 👋
-        </Text>
-        <Text
-          style={{
-            fontFamily: 'Pretendard-Medium',
-            fontSize: 16,
-            marginTop: 7,
-            color: themes.light.textColor.Primary50,
-          }}>
-          이름을 입력해주세요.
-        </Text>
-      </Container1>
-      <Container2>
-        <InputContainer marginRight="5px">
-          <TextInput
-            placeholder="성"
-            value={lastName}
-            onChangeText={setLastName}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <ProgressBar progress={progress} />
+        <Container1>
+          <Text
+            style={{
+              fontFamily: fonts.title.fontFamily,
+              fontSize: fonts.title.fontSize,
+            }}>
+            안녕하세요, 메디지입니다 👋
+          </Text>
+          <Text
+            style={{
+              fontFamily: 'Pretendard-Medium',
+              fontSize: 16,
+              marginTop: 7,
+              color: themes.light.textColor.Primary50,
+            }}>
+            이름을 입력해주세요.
+          </Text>
+        </Container1>
+        <Container2>
+          <InputContainer marginRight="5px">
+            <TextInput
+              placeholder="성"
+              placeholderTextColor={themes.light.textColor.placeholder}
+              value={lastName}
+              onChangeText={setLastName}
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                // focus next input
+                secondInput.current.focus();
+              }}
+            />
+          </InputContainer>
+          <InputContainer marginLeft="5px">
+            <TextInput
+              placeholder="이름"
+              placeholderTextColor={themes.light.textColor.placeholder}
+              value={firstName}
+              onChangeText={setFirstName}
+              returnKeyType="done"
+              onSubmitEditing={Keyboard.dismiss}
+              ref={secondInput}
+            />
+          </InputContainer>
+        </Container2>
+        <BtnContainer>
+          <BackAndNextButtons
+            onPressPrev={() => navigation.goBack()}
+            onPressNext={handleNext}
           />
-        </InputContainer>
-        <InputContainer marginLeft="5px">
-          <TextInput
-            placeholder="이름"
-            value={firstName}
-            onChangeText={setFirstName}
-          />
-        </InputContainer>
-      </Container2>
-      <BtnContainer>
-        <BackAndNextButtons
-          onPressPrev={() => navigation.goBack()}
-          onPressNext={handleNext}
-        />
-      </BtnContainer>
-    </Container>
+        </BtnContainer>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 };
 export default SignUpNameScreen;
