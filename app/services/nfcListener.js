c33207980473a2f8c67b87be231a98ef0dbe47dd
hjuump <butter0488@gmail.com>
2025-04-05 16:35:03 +0900

    refactor: 커스텀 모달 분리

--- app/services/nfcListener.js
+++ app/services/nfcListener.js
@@ -1,25 +1,49 @@
-import {useEffect} from 'react';
-import {Alert} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Platform} from 'react-native';
 import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
+import CustomModal from '../components/CustomModal';
+import FontSizes from '../../assets/fonts/fontSizes';
+import {themes} from '../styles';
+import styled from 'styled-components/native';
 
 const useNfcListener = () => {
+  // const [nfcTag, setNfcTag] = useState(null);
+  const [nfcTag, setNfcTag] = useState(true); // 모달 항상 표시
+
   useEffect(() => {
     NfcManager.start();
 
     NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
-      Alert.alert('NFC 태그 감지됨!', 'NFC가 인식됐어요!');
       console.log('태그:', tag);
-
+      setNfcTag(tag); // 태그 저장해서 모달 띄움
       NfcManager.unregisterTagEvent().catch(() => null);
     });
 
-    NfcManager.registerTagEvent({alertMessage: '최강 메디지!!'});
+    NfcManager.registerTagEvent({
+      alertMessage: Platform.OS === 'ios' ? '최강 메디지!!' : undefined,
+    });
 
     return () => {
       NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
       NfcManager.unregisterTagEvent().catch(() => null);
     };
   }, []);
+
+  const ModalComponent = () =>
+    nfcTag ? (
+      <CustomModal visible={!!nfcTag} onClose={() => setNfcTag(null)}>
+        <Title>NFC 태그 감지됨</Title>
+      </CustomModal>
+    ) : null;
+
+  return {ModalComponent};
 };
 
 export default useNfcListener;
+
+const Title = styled.Text`
+  font-size: ${FontSizes.title.default};
+  font-family: 'KimjungchulGothic-Bold';
+  color: ${themes.light.textColor.textPrimary};
+  padding: 30px;
+`;
