a9fd85cc50014689ae3dd0699e76ea6645480687
kimgazii <gy0320ya@naver.com>
2025-04-24 00:47:55 +0900

    style: CustomCalendar 스타일 수정

--- app/components/CustomCalendar.js
+++ app/components/CustomCalendar.js
@@ -4,65 +4,67 @@ import DateTimePicker, { useDefaultStyles } from 'react-native-ui-datepicker';
 import { fonts, themes } from '../styles';
 import FontSizes from '../../assets/fonts/fontSizes';
 
-export const CustomCalendar = ({ 
-    selectedDate, 
-    onDateChange, 
-    mode = 'single', 
+export const CustomCalendar = ({
+  selectedDate,
+  onDateChange,
+  mode = 'single',
 }) => {
-    const defaultStyles = useDefaultStyles();
-    const [selected, setSelected] = useState(selectedDate);
-  
-    return (
+  const defaultStyles = useDefaultStyles();
+  const [selected, setSelected] = useState(selectedDate);
+
+  return (
+    <CalendarContainer>
       <DateTimePicker
         mode={mode}
         date={selected}
         onChange={({ date }) => {
-            console.log('선택된 날짜:', date);
-            setSelected(date);
-            onDateChange(date);
-          }}
-
+          console.log('선택된 날짜:', date);
+          setSelected(date);
+          onDateChange(date);
+        }}
         styles={{
-            ...defaultStyles,
-            container: {
-                ...defaultStyles.container,
-            },
-            month: {
-                ...defaultStyles.month,
-            },
-            monthText: {
-                ...defaultStyles.monthText,
-            },
-            weekdays: {
-              ...defaultStyles.weekdays,
-            },
-            weekday: {
-                ...defaultStyles.weekday,
-            },
-            days: {
-              ...defaultStyles.days,
-            },
-            day: {
-                width: 46,
-                height: 46,
-                borderRadius: 50,
-                justifyContent: 'center',
-                alignItems: 'center'
-            },
-            dayLabel: {
-              fontSize: FontSizes.caption.default,
-            },
-            today: {
-                ...defaultStyles.today,
-            },
-            selected: {
-                backgroundColor: themes.light.pointColor.Primary,
-            },
-            selected_label: {
-                color: themes.light.textColor.buttonText,
-            },
-          }}
+          ...defaultStyles,
+          today: { 
+            backgroundColor: themes.light.bgColor.bgSecondary,
+            borderRadius: 30,
+          },
+          selected: { 
+            backgroundColor: themes.light.pointColor.Primary,
+            borderRadius: 30, 
+          },
+          selected_label: {
+            fontFamily: `Pretendard-SemiBold`,
+            fontSize: FontSizes.body.default,
+            color: themes.light.textColor.buttonText,
+          },
+          day_label: {
+            fontFamily: `Pretendard-SemiBold`,
+            fontSize: FontSizes.body.default,
+            color: themes.light.textColor.textPrimary,
+          },
+          weekday_label: {
+            fontFamily: `Pretendard-Medium`,
+            fontSize: FontSizes.caption.default,
+            color: themes.light.textColor.textPrimary,
+          },
+          year_selector_label: {
+            fontFamily: `Pretendard-Bold`,
+            fontSize: FontSizes.title.default,
+            color: themes.light.textColor.textPrimary,
+          },
+          month_selector_label: {
+            fontFamily: `Pretendard-Bold`,
+            fontSize: FontSizes.title.default,
+            color: themes.light.textColor.textPrimary,
+          },
+        }}
         locale="ko"
       />
-    );
+    </CalendarContainer>
+  );
 };
+
+const CalendarContainer = styled.View`
+  background-color: ${themes.light.bgColor.bgPrimary};
+`;
+
