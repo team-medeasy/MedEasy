98a1eaa224e95cd1db01e240b77bd46580ed0485
kimgazii <gy0320ya@naver.com>
2025-04-28 23:12:00 +0900

    feat: 마이페이지 리스트 헤더 추가

--- app/screens/Settings/Announcements.js
+++ app/screens/Settings/Announcements.js
@@ -3,12 +3,12 @@ import {View, Text} from 'react-native';
 import styled from 'styled-components/native';
 import {themes} from '../../styles';
 import FontSizes from '../../../assets/fonts/fontSizes';
+import { Header } from '../../components';
 
 const Profile = () => {
   return (
     <Container>
-      <Title>공지</Title>
-      <Text>공지 내용</Text>
+      <Header>공지사항</Header>
     </Container>
   );
 };
@@ -16,14 +16,6 @@ const Profile = () => {
 const Container = styled.View`
   flex: 1;
   background-color: ${themes.light.bgColor.bgPrimary};
-  padding: 20px;
-`;
-
-const Title = styled.Text`
-  font-size: ${FontSizes.title.default};
-  font-family: 'KimjungchulGothic-Bold';
-  color: ${themes.light.textColor.buttonText};
-  margin-bottom: 20px;
 `;
 
 export default Profile;
